734f72248812994efdae69eae311c1329feb2af9
Sokhavuth <vuthdevelop@gmail.com>
2022-07-07 12:53:12 +0700

    Make it better

--- controllers/front/index.py
+++ controllers/front/index.py
@@ -2,11 +2,12 @@
 
 from sanic import Sanic
 from sanic_ext import render
+from copy import deepcopy
 
 class Index():
     def __init__(self):
         app = Sanic.get_app('Multinews')
-        self.config = app.config
+        self.config = deepcopy(app.config)
 
     async def getItem(self,req):
         self.config["pageTitle"] = 'ទំព័រ​ដើម'
--- controllers/front/login.py
+++ controllers/front/login.py
@@ -2,11 +2,12 @@
 
 from sanic import Sanic
 from sanic_ext import render
+from copy import deepcopy
 
 class Login():
     def __init__(self):
         app = Sanic.get_app('Multinews')
-        self.config = app.config
+        self.config = deepcopy(app.config)
 
     async def getItem(self,req):
         self.config["pageTitle"] = 'ទំព័រ​ចុះ​ឈ្មោះ​ចូល​​ក្នុង'
