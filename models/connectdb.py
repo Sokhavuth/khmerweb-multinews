df56a149804a59080795174c3e3f72bec3ad2323
Sokhavuth <vuthdevelop@gmail.com>
2022-07-20 17:54:55 +0700

    Make it better

--- models/connectdb.py
+++ models/connectdb.py
@@ -1,6 +1,6 @@
 # models/connectdb.py
 # pip install python-dotenv
-# pip install pymongo
+# pip install motor
 
 import os
 from dotenv import load_dotenv  
@@ -8,12 +8,12 @@ load_dotenv()
 
 import asyncio
 import motor.motor_asyncio
+
 mydb = None
+
 async def get_server_info():
     global mydb
-    # replace this with your MongoDB connection string
     conn_str = os.getenv('DATABASE_URI')
-    # set a 5-second connection timeout
     client = motor.motor_asyncio.AsyncIOMotorClient(conn_str, serverSelectionTimeoutMS=5000)
     mydb = client[os.getenv('DB_NAME')]
     
--- routes/admin/post.py
+++ routes/admin/post.py
@@ -2,10 +2,14 @@
 
 from sanic import Sanic
 from controllers.admin.post import Post
+from sanic.response import redirect
  
 app = Sanic.get_app('Multinews')
-instance = Post()
+post = Post()
  
 @app.get("/admin/post")
 async def getItem(req):
-    return await instance.getItem(req)
+    if(req.ctx.session.get('user')):
+        return await post.getItem(req)
+    else:
+        return redirect('/login')
